c341e4bb7933eed446c50fe0e3979793516f0590
supriadiii <supriadiadi1001@gmail.com>
2023-09-19 15:16:31 +0700

    first commit

---
+++ Perkondisian.js
@@ -0,0 +1,19 @@
+// contoh satu
+const isRaining = true
+
+console.log("persiapan sebelum berangkat sekolah")
+
+if(isRaining){
+    console.log("hari ini akan hujan, bawa payung")
+}
+console.log("berangkat sekolah")
+
+// contoh 2 
+let x = 50
+if(x>70 ){
+    console.log(x)
+}else{
+    console.log("nilai x kurang dari 70")
+}
+
+
